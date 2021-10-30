6c8fb7d295151a6c3dc657ea3d1c4c5eb688083c
Gigi <giselaruzafamado@gmail.com>
2021-10-30 13:34:30 +0200

    init

---
+++ src/main/kotlin/com/test/apipost/ApipostApplication.kt
@@ -0,0 +1,11 @@
+package com.test.apipost
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApipostApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApipostApplication>(*args)
+}
---
+++ src/test/kotlin/com/test/apipost/ApipostApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.apipost
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApipostApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
